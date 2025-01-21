c1c9f2755df168b3dbb074006ba9235672dd348b
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-21 15:21:35 -0500

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <footer>Coded by <a href="https://github.com/MutableSpectre" target="_blank"><strong>Stephanie Ratliff-Putney</strong></a>, 
+      open sourced on <a href="https://github.com/MutableSpectre/srp-world-clock-project" target="_blank">Github</a>, 
+      and hosted on <a href="https://srp-world-clock-project.netlify.app/" target="_blank">Netlify</a>.
+      </footer>
     </div>
   );
 }
