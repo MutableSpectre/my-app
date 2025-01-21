360f9e4b16b43e56b6388ce2569882ffbd204a68
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-21 16:03:44 -0500

    Added "noreferrer" to footer links and updated links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <footer>Coded by <a href="https://github.com/MutableSpectre" target="_blank" rel="noopener"><strong>Stephanie Ratliff-Putney</strong></a>, 
-      open sourced on <a href="https://github.com/MutableSpectre/srp-world-clock-project" target="_blank" rel="noopener">Github</a>, 
-      and hosted on <a href="https://srp-world-clock-project.netlify.app/" target="_blank" rel="noopener">Netlify</a>.
+      <footer>Coded by <a href="https://github.com/MutableSpectre" target="_blank" rel="noopener noreferrer"><strong>Stephanie Ratliff-Putney</strong></a>, 
+      open sourced on <a href="https://github.com/MutableSpectre/my-app" target="_blank" rel="noopener noreferrer">Github</a>, 
+      and hosted on <a href="https://shecodes-weatherreactapp.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.
       </footer>
     </div>
   );
