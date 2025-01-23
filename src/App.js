aeca6df7bbd651559c60fa6693ec4584a79c94f2
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-23 12:15:01 -0500

    Updated React and added code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="Container">
+        <Weather />
       <footer>Coded by <a href="https://github.com/MutableSpectre" target="_blank" rel="noopener noreferrer"><strong>Stephanie Ratliff-Putney</strong></a>, 
       open sourced on <a href="https://github.com/MutableSpectre/my-app" target="_blank" rel="noopener noreferrer">Github</a>, 
       and hosted on <a href="https://shecodes-weatherreactapp.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.
       </footer>
+      </div>
     </div>
   );
 }
