5cc4c60b141f1b4fd3b94ad46e5b7cee78b66531
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-26 16:47:54 -0500

    Added Day 1 Forecast

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5da7b2dc058f07286fea39c4cee516a";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
